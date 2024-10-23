7fa84fca75f333e55a1a25ad73560d99ec7a45f0
tammykhan <tammykhan543@gmail.com>
2024-10-23 13:20:20 +0600

    set layout and make react responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
   return (
     <>
       <Header></Header>
+      <div className='md:flex'>
       <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
